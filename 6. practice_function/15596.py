6711b3b4f79ffca08cf0137b558ccb8c6a22d695
hyebinkang <khb1029@naver.com>
2022-03-17 15:40:21 +0900

    함수 작성 수정
    a가 리스트 일 때
    a가 정수일 때

--- 6. practice_function/15596.py
+++ 6. practice_function/15596.py
@@ -1,3 +1,4 @@
+#-----------리스트일 때
 def solve(a):
     ans = 0
     for i in range(len(a)): #a의 개수
@@ -5,3 +6,11 @@ def solve(a):
     return ans
 
 solve([1,2,3,4,5])
+#-----------정수일 때
+def solve1(a):
+    ans = 0
+    for i in range(a+1): #a의 개수
+        ans += i #a: list이기 때문에 a[i]
+    return ans
+
+print(solve1(5))
