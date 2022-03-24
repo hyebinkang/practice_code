5da1c03316e422761d4fb5dffa112d21300b329f
hyebinkang <khb1029@naver.com>
2022-03-24 23:31:08 +0900

    주석 수정

--- 8. practice_mathmatics1/1712.py
+++ 8. practice_mathmatics1/1712.py
@@ -4,9 +4,10 @@ if varied >= now :  #가변비용이 현재 비용보다 크다면
     print(-1)
 else:   #현재비용 - 가변비용을 고정비용으로 나눈 후 +1
     print((fixed // (now-varied)) + 1)
+
 # fixed, varied, now = map(int, input().split())
 #
-# for i in range(fixed):
+#for i in range(now):
 #     cost = now * i
 #     produce = fixed + varied * i
 #     if(cost > produce):
