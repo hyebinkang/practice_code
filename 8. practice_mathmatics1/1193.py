61ac97e249d85736715d50660d6dce4252f3593e
hyebinkang <khb1029@naver.com>
2022-03-30 22:32:31 +0900

    분수 순서 구하기
    코드 수정

--- 8. practice_mathmatics1/1193.py
+++ 8. practice_mathmatics1/1193.py
@@ -1,42 +1,17 @@
-n = int(input())
+n = int(input())    #n번째 수 입력
 
-x = 1   #분자
-y = 1   #분모
-
-cross = 1
-avg_cross = 0
-
-
+cross = 0           #사선 횟수
+avg_cross = 0       #사선 안에서 최대의 값을 의미
 while n > avg_cross:
+    cross +=1           #사선 횟수 +1
+    avg_cross += cross  #사선 횟수 + avg_cross = 사선 안에서 최대의 값을 의미하게 된다.
 
-    avg_cross += 1
+gap = avg_cross - n     #최대의 값에서 -n
+if cross % 2 == 0:      #사선이 짝수번째 이면
+    x = cross - gap     #분자
+    y = gap + 1         #분모
+else:
+    x = gap +1          #분자
+    y = cross -gap      #분모
 
-    for count in range(1, cross+1):
-        if(cross % 2 == 0):
-            if(count % 2 ==0):
-                x = avg_cross-cross +1
-                y = cross
-                break
-            else:
-                x = cross
-                y = abs(avg_cross-cross)
-                break
-
-        else:
-            if (count % 2 == 0):
-                x = avg_cross - cross +1
-                y = cross
-                break
-            else:
-                x = cross
-                y = abs(avg_cross - cross) +1
-                break
-    count += 1
-    if(count == cross):
-        cross +=1
-        count = 1
-
-
-
-
-print(x,'/',y, sep='')
+print(x,'/',y, sep='')  #결과 출력
