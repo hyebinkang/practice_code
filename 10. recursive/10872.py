09d98b87abdac434c01ad31fd3b8cdab4de410fc
hyebinkang <khb1029@naver.com>
2022-04-12 18:40:54 +0900

    팩토리얼 계산
    for문 사용 코드 추가

--- 10. recursive/10872.py
+++ 10. recursive/10872.py
@@ -5,3 +5,11 @@ def factorial(num):
         return 1                            #0일 때 1반환
 num = int(input())
 print(factorial(num))                       #결과 출력
+
+
+# #---for문----
+# n = int(input())
+# result = 1
+# for i in range(1, n+1):
+#     result *=i              #result 에 1부터 곱하기
+#     print(result)
