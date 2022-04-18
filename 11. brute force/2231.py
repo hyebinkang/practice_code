2282b9895286c77e979f24e4b8d83bcbddd5cfc6
hyebinkang <khb1029@naver.com>
2022-04-18 19:15:51 +0900

    분해합
    주석추가

--- 11. brute force/2231.py
+++ 11. brute force/2231.py
@@ -1,11 +1,11 @@
 n = int(input())
 result = 0
-for i in range(1, n+1):
-    m = list(map(int, str(i)))
-    result = i+sum(m)
+for i in range(1, n+1):             #1부터 n 까지
+    m = list(map(int, str(i)))      #map함수를 사용하여, str 하나씩 나누어 int로 저장
+    result = i+sum(m)               #i+ 리스트의 합(= 입력받은 숫자 n을 각 자리수로 나눈것)
 
-    if(result == n):
-        print(i)
+    if(result == n):                #result 와 n이 같다면
+        print(i)                    #i출력 = 분해합
         break
-else:
+else:                               #숫자n 까지에도 만족하는 수가 없다면 0출력
     print(0)
