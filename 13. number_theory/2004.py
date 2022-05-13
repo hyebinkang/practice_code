e60f78441d98bbe5c8bb6827df25e1aa429083be
hyebinkang <khb1029@naver.com>
2022-05-13 17:45:46 +0900

    조합 0의 개수
    이항계수로 구한 수의 0의 개수를 출력하기

--- 13. number_theory/2004.py
+++ 13. number_theory/2004.py
@@ -1,27 +1,37 @@
 import math
 import sys
-
-num_list = []
-count = 0
+#
+# num_list = []
+# count = 0
+#
+# n, m = map(int, sys.stdin.readline().split())
+#
+# num = math.factorial(n)//(math.factorial(m)*math.factorial(n-m))
+#
+# while num !=0:
+#     a = num % 10
+#     if a !=0:
+#         print(count)
+#         break
+#     else:
+#         count +=1
+#         num_list.append(num % 10)
+#         new_num = num//10
+#         num = new_num
 
 n, m = map(int, sys.stdin.readline().split())
 
-num = math.factorial(n)//(math.factorial(m)*math.factorial(n-m))
+def two(n):                 #10은 2와 5의 곱으로 이루어짐
+    n2 = 0                  #즉, n,m이 가지고 있는 2와 5의 개수, m-n이 가지고 있는 2와 5의 개수를 따진다
+    while(n!=0):
+        n//=2
+        n2 += n
+    return n2
+def five(n):
+    n5 = 0
+    while(n!=0):
+        n//=5
+        n5 += n
+    return n5
 
-while num !=0:
-    a = num % 10
-    if a !=0:
-        print(count)
-        break
-    else:
-        count +=1
-        num_list.append(num % 10)
-        new_num = num//10
-        num = new_num
-
-# for i in num_list:
-#     if i == 0:
-#         count +=1
-#     else:
-#         print(count)
-#         break
+print(min(two(n)-two(m)-two(n-m), five(n)-five(m)-five(n-m)))
