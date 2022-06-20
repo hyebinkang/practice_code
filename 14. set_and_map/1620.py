88758ad589d2cb89a474af88734c12c91cb91ac5
hyebinkang <khb1029@naver.com>
2022-06-20 15:26:44 +0900

    나는야 포켓몬 마스터 이다솜
    주석추가

--- 14. set_and_map/1620.py
+++ 14. set_and_map/1620.py
@@ -5,13 +5,13 @@ arr = {}
 
 for _ in range(n):
     put = sys.stdin.readline().strip()
-    arr[put] = _
-
-arr_keys = list(arr.keys())
-
+    arr[put] = _                        #arr의 key: 포켓몬 이름, value: 포켓몬 번호
+print(arr)
+arr_keys = list(arr.keys())             #arr의 key 값 만을 가지는 리스트를 생성
+print(arr_keys)
 for i in range(m):
-    answer = sys.stdin.readline().strip()
-    if answer.isdigit():
+    answer = sys.stdin.readline().strip()   #이름, 숫자를 입력할 때
+    if answer.isdigit():                    #숫자이면 key값만 있는 arr_keys에서 해당하는 리스트
         print(arr_keys[int(answer)-1])
     else:
         print(arr[answer]+1)
