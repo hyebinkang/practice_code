40871b6d538d277455efc7f1f41446bb1a8cdf53
hyebinkang <khb1029@naver.com>
2022-06-21 17:38:28 +0900

    서로 다른 부분 문자열의 개수
    주석 추가

--- 14. set_and_map/11478.py
+++ 14. set_and_map/11478.py
@@ -2,8 +2,8 @@ s = input()
 
 arr=[]
 
-for i in range(len(s)):
+for i in range(len(s)):             #s의 길이만큼 반복
     for j in range(len(s)+1):
-        arr.append(s[j:j+i])
+        arr.append(s[j:j+i])        #처음부터 1씩 늘어갈 때 마다 append
 
-print(len(set(arr)))
+print(len(set(arr)))                #arr을 중복제거한 길이를 출력
